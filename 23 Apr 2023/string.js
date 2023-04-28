e1c1686434bd172a5bd10dcd3c842a3f1989fd73
Simeon <simeon1929@gmail.com>
2023-04-28 15:22:57 +0500

    string types checke

---
+++ 23 Apr 2023/string.js
@@ -0,0 +1,32 @@
+console.log('string worked');
+
+let Name = 'simeon'
+let age = 12
+
+// concat string 
+let result = Name + ' is ' + age + ' years old'
+console.log(result);
+
+// string other type
+console.log('Template string');
+let newResult = `your name is  ${Name}  and age ${age}`
+console.log(newResult);
+
+// create html 
+
+
+
+let htmlUse = `<h1> your name is ${Name}</h1>`
+console.log(htmlUse);
+
+// alert('hello')
+// const elem = document.querySelector(".myclass");
+// console.log(elem);
+
+// let h1 = document.querySelectorAll('h1')
+// console.log(h1);
+const matches = document.querySelector("p");
+console.log(matches);
+
+let idVal = document.getElementById('simeon')
+console.log(idVal);
