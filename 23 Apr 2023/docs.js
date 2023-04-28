e94271a93abd3e6aa4d167a1826dcb59a7e3512d
Simeon <simeon1929@gmail.com>
2023-04-28 16:13:54 +0500

    document tested

---
+++ 23 Apr 2023/docs.js
@@ -0,0 +1,11 @@
+let h1Tag = document.querySelector('h1')
+console.log(h1Tag);
+
+// Template string injected to document file
+let FName = 'simeon'
+let age = 30
+
+let html = `${FName}`
+html.innerText = h1Tag
+
+
