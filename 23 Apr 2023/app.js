f044ba3816ac45758a587298deeb322f415baea2
Simeon <simeon1929@gmail.com>
2023-04-28 14:17:57 +0500

    home try

---
+++ 23 Apr 2023/app.js
@@ -0,0 +1,73 @@
+// console.log('Simeon');
+
+// let x = 20 
+// console.log('let first X :', x);
+
+// x = 40
+
+// console.log('let first x ', x);
+
+// var y = 50
+// console.log('Var first y', y);
+// y = 100
+
+// console.log('Var first y', y);
+
+
+// const z = 100
+// console.log('COnst z ', z);
+
+
+// console.log('COnst z ', z);
+
+// var xy = ''
+// console.log(xy);
+
+// let ax = 20
+
+// ax += 20 
+
+// console.log(ax);
+
+// let mulply = 5 
+// mulply *=2
+
+// console.log('multiply' , mulply);
+
+
+// console.log('Nan using , Nan means Not a number');
+// console.log( 3 / 'simeon');
+
+
+// // Template string way 
+
+// const Name = "Simeon received"
+// const numberL = 20
+// const likes = 'likes'
+
+// // conncation 
+
+// let concaT =Name + ' ' + numberL +" "+ likes
+// console.log('Result concat : ', concaT);
+
+// Template string way 
+// let tempLa = `<h1>${Name + numberL + likes}</h1>`
+
+let html = document.getElementById("myP");
+console.log(html);
+
+
+console.log('simeon');
+let el = document.querySelector("myclass");
+console.log(el);
+// body.innnerHtml = tempLa
+// console.log(tempLa);
+
+
+
+
+
+
+
+
+
